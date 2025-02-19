76e4a197c40a09f7c3ab7d15aa1adb2b9abf959c
PedroFerreira4 <pedro.silva81690@gmail.com>
2025-02-19 10:08:38 -0300

    19.02 - variaveis

---
+++ aula2 variaveis/src/aula2variaveis/FunçoesMath.java
@@ -0,0 +1,23 @@
+package aula2variaveis;
+
+public class FunçoesMath {
+
+	public static void main(String[] args) {
+		double potencia = Math.pow(2, 3);
+		System.out.println(potencia);
+		
+		double raisQuadrada = Math.sqrt(16);
+		System.out.println(raisQuadrada);
+		
+		double numeroAleatorio = Math.random();
+		System.out.printf("%.1f" , numeroAleatorio);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ aula2 variaveis/src/aula2variaveis/tipodedados.java
@@ -0,0 +1,24 @@
+package aula2variaveis;
+
+public class tipodedados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int idade = 50;
+			long ano = 2025; 
+			
+			System.out.println(idade);
+			System.out.print(ano);
+			
+			float peso = 70.5f;
+			double altura = 78.5;
+			
+			System.out.println(peso);
+			System.out.print(altura);
+			
+			String nomeCompleto = "Douglas Morais";
+			System.out.println(nomeCompleto);
+			
+	}
+
+}
